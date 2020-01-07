30059629d4f9fc53ce111d18f27eeb14b673515f
Artur Gorzelany <artasss47@gmail.com>
2020-01-07 20:18:39 +0100

    Integracja WPF

--- Tetris/WPF Front/MainWindow.xaml.cs
+++ Tetris/WPF Front/MainWindow.xaml.cs
@@ -13,6 +13,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Drawing;
+using System.Windows.Forms;
 
 namespace WPF_Front
 {
@@ -22,23 +24,31 @@ namespace WPF_Front
     public partial class MainWindow : Window
     {
         public Tetris tetris;
+        private SolidBrush brush;
         public MainWindow()
         {
             InitializeComponent();
+            //this.KeyPreview = true;
+            tetris = new Tetris(10, 20, new TetrisEventImp(this));
+            //brush = new SolidBrush(Color.Gray);
         }
 
         private void StartBtn_Click(object sender, RoutedEventArgs e)
         {
-            Button btn = (Button)sender;
-           
+            //game.Focus(); 
+            System.Windows.Controls.Button btn = (System.Windows.Controls.Button)sender;
+
             if (btn.Content.ToString() == "Start Game")
             {
                 //do smth when Start Game button is pressed
+                tetris.start();
                 btn.Content = "Pause Game";
+
             }
             else
             {
                 //do smth when Pause Game button is pressed
+                tetris.pause();
                 btn.Content = "Start Game";
             }
 
@@ -47,7 +57,60 @@ namespace WPF_Front
         private void ResetBtn_Click(object sender, RoutedEventArgs e)
         {
             //do smth when Reset Game button is pressed
+            tetris.setUp();
             startBtn.Content = "Start Game";
+        }
+
+        private void game_Paint(object sender, PaintEventArgs e)
+        {
+            int rectSize = 30;
+            for (int x = 0; x < tetris.Width; x++)
+            {
+                for (int y = 0; y < tetris.Height; y++)
+                {
+                    Field field = tetris.Board[x, y];
+                    brush.Color = field.Color;
+                    e.Graphics.FillRectangle(brush, x * rectSize, y * rectSize, rectSize, rectSize);
+                }
+            }
+        }
+
+        class TetrisEventImp : TetrisEvent
+        {
+            MainWindow mainWindow;
+
+            public TetrisEventImp(MainWindow game)
+            {
+                this.mainWindow = game;
+            }
+
+            public void onBlockChange()
+            {
+                //mainWindow.game.Invalidate();
+            }
+
+            public void onGameOver()
+            {
+                System.Windows.MessageBox.Show("Przegrana");
+            }
+
+            public void onPointsChange(int points, int tetris)
+            {
+                //gameView.lines.Text = points.ToString();
+            }
+        }
+
+        private void GameView_KeyDown(object sender, System.Windows.Forms.KeyEventArgs e)
+        {
+            switch (e.KeyCode)
+            {
+                case Keys.A: tetris.moveLeft(); break;
+                case Keys.D: tetris.moveRight(); break;
+                case Keys.S: tetris.moveDown(); break;
+                case Keys.W: tetris.rotate(); break;
+                case Keys.Space: tetris.flor(); break;
+            }
+
         }
     }
 }
